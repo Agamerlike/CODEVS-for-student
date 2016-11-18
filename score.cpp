c89f39122f883aff7ceba6a207cd861e59730f23
MN K <MN K>
2016-11-18 14:53:33 +0900

    first

---
+++ score.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math>
+
+void main()
+{
+	int score=0;
+	int ojama=0;
+	int chain=0;
+	int cnt;
+	while(cnt > 0)
+	{
+		chain++;
+		cout << "chain" << chain << endl;
+		cin << cnt << endl;
+		score += floor(pow(1.3, chain)) + floor((double)cnt / 2);
+	}
+	ojama = floor((double)score/5);
+	cout << "score:" << score << endl;
+	cout << "ojama:" << ojama << endl;
+}
