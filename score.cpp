32892461e0277c2048b56dc2efb4f1a5b32d9489
MN K <MN K>
2016-11-18 19:16:41 +0900

    slight modify

--- score.cpp
+++ score.cpp
@@ -1,14 +1,3 @@
-/*スコアの上では同時大量消し<<連鎖数
- *よって2消しを連続させる方法が最も有効
- *17連鎖あたりを狙うのがベストか？（先制攻撃なら）
- *10連鎖2消し...おじゃま15個
- *15連鎖2消し...おじゃま58個
- *16連鎖2消し...おじゃま75個
- *17連鎖2消し...おじゃま98個
- *18連鎖2消し...おじゃま127個
- *19連鎖2消し...おじゃま165個
- *20連鎖2消し...おじゃま215個
- */
 #include <iostream>
 #include <string>
 using namespace std;
@@ -23,6 +12,10 @@ void main()
 		chain++;
 		cout << "chain" << chain << endl;
 		cin >> cnt;
+		if(cnt == 0)
+		{
+			break;
+		}
 		score += floor(pow(1.3, chain)) + floor((double)cnt / 2);
 	}
 	ojama = floor((double)score/5);
